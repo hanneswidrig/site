dc809dab9132b604dcfe51cba4b731950a3b81a0
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2023-09-11 08:48:20 -0400

    chore: reformat

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+export default {
+	useTabs: true,
+	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+	overrides: [{ files: "*.astro", options: { parser: "astro" } }],
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-
 /** @type {import("tailwindcss").Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -12,11 +10,6 @@ export default {
 			blue: "#253D5B",
 			green: "#355834",
 			purple: "#564D7C",
-		},
-		extend: {
-			fontFamily: {
-				display: ["Inter", ...defaultTheme.fontFamily.sans],
-			},
 		},
 	},
 	plugins: [],
