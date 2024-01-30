34ef670644cd7b905f41af64d0ce25483f9b10b2
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2024-01-29 20:03:11 -0500

    feat: simplify components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,16 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		colors: {
-			transparent: "transparent",
-			default: "#F6F8FF",
-			white: "#FFFFFF",
-			red: "#CF3B3B",
-			blue: "#253D5B",
-			green: "#355834",
-			purple: "#564D7C",
+		extend: {
+			colors: {
+				transparent: "transparent",
+				default: "#F6F8FF",
+				white: "#FFFFFF",
+				red: "#CF3B3B",
+				blue: "#253D5B",
+				green: "#355834",
+				purple: "#564D7C",
+			},
 		},
 	},
 	plugins: [],
