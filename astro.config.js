163ec49f2333d5ad2e4c3a2af26297eedbb4949f
Hannes Widrig <hanneswidrig@gmail.com>
2026-07-15 11:33:59 -0400

    feat: switch to latex style

--- astro.config.js
+++ astro.config.js
@@ -1,8 +1,44 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "CMU Serif",
+			cssVariable: "--font-cmu",
+			fallbacks: ["serif"],
+			options: {
+				variants: [
+					{
+						weight: 400,
+						style: "normal",
+						src: ["computer-modern/fonts/cmu-serif-500-roman.woff2"],
+						display: "block",
+					},
+					{
+						weight: 400,
+						style: "italic",
+						src: ["computer-modern/fonts/cmu-serif-500-italic.woff2"],
+						display: "block",
+					},
+					{
+						weight: 700,
+						style: "normal",
+						src: ["computer-modern/fonts/cmu-serif-700-roman.woff2"],
+						display: "block",
+					},
+					{
+						weight: 700,
+						style: "italic",
+						src: ["computer-modern/fonts/cmu-serif-700-italic.woff2"],
+						display: "block",
+					},
+				],
+			},
+		},
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
